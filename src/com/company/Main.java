b7c23ccceb2dd0ba76b530b8df13d575c0279dbd
AndrewPirogovskyi <andrew.wkspace@gmail.com>
2023-04-27 13:37:20 +0300

    Initial version

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ import java.io.File;
 class Main {
     public static void main(String[] args) {
         // Path to the file you want to delete
-        String FilePath = "E:/Games/Steam/steamapps/common/Deep Rock Galactic/FSD/Saved/Config/WindowsNoEditor/GameUserSettings.ini";
+        String FilePath = "E:";
 
         // Create a File object for the file to be deleted
         File object = new File(FilePath);
